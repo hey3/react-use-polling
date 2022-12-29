8b417faaf418fa5f7746b63f32d6ca47f9c88def
hey3 <oh1kohei@gmail.com>
2022-12-29 16:35:34 +0000

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,67 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2022: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  ignorePatterns: ['node_modules/*', 'dist'],
+  extends: ['eslint:recommended', 'plugin:import/recommended'],
+  plugins: ['unused-imports'],
+  rules: {
+    'import/export': 'off',
+    'import/newline-after-import': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
+        alphabetize: { order: 'asc' },
+      },
+    ],
+    'unused-imports/no-unused-imports': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts', '.tsx'],
+        paths: ['node_modules/', 'node_modules/@types'],
+      },
+    },
+  },
+  overrides: [
+    {
+      files: ['**/*.[jt]sx'],
+      settings: { react: { version: 'detect' } },
+      env: {
+        browser: true,
+      },
+      extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended'],
+      rules: {
+        'react/prop-types': 'off',
+        'react/react-in-jsx-scope': 'off',
+      },
+    },
+    {
+      files: ['**/*.ts?(x)'],
+      parser: '@typescript-eslint/parser',
+      extends: ['plugin:@typescript-eslint/recommended', 'plugin:import/typescript'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': ['error'],
+        '@typescript-eslint/explicit-function-return-type': [
+          'warn',
+          {
+            allowExpressions: true,
+            allowConciseArrowFunctionExpressionsStartingWithVoid: true,
+          },
+        ],
+      },
+    },
+    {
+      files: ['**'],
+      extends: ['prettier'],
+    },
+  ],
+}
